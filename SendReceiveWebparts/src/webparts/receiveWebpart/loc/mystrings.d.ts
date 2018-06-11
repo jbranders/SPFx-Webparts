2ddda7f9d56b986f4d7c6dff0bbcfc2f3a60809e
Jeroen Branders <jeroen.branders@lineas.net>
2018-06-11 13:22:47 +0200

    New webpart added

---
+++ SendReceiveWebparts/src/webparts/receiveWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReceiveWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReceiveWebpartWebPartStrings' {
+  const strings: IReceiveWebpartWebPartStrings;
+  export = strings;
+}
